d1d6dd2468c9ddb80c8b5bafa18efaae4d11ccc5
swethamadhavi <swethamadhaviyadagiri2001@gmail.com>
2022-06-20 14:39:29 +0530

    Assesment4

---
+++ Assesment4.py
@@ -0,0 +1,60 @@
+#x=[1,2,3,4,5,3,5]
+#print(len(x))
+#x=set(x)
+#print(x)
+#x=list(x)
+#print(x)
+
+#x=[1,2,3]
+#x[0]=10
+#x[1]=20
+#x[2]=30
+#print(x)
+
+#x=range(2,255,2)
+#for i in x:
+ #print(i,end=" ")
+
+#x = range(1, 255, 2)
+#for i in x:
+# print(i, end=" ")
+
+#x=(1,2,3,4)
+#x=list(x)
+#x[3]=47
+#x[1]=20
+#print(x)
+#x=tuple(x)
+#print(x)
+
+#x=[1,2,3]
+#print(len(x))
+#print(x[::-1])
+#x.reverse()
+#print(x)
+
+#a=10
+#b=20
+#print(a,b)
+#a,b=b,a
+#print("a value is",a)
+#print("b value is",b)
+
+#a=16
+#print(bin(a))
+
+#x={'name':"swetha",'address':"vijayawada",'phoneno':2345678912}
+#print(len(x))
+#print(x.values())
+
+#x={0}
+#print(type(x))
+#y={'A':1,'B':124}
+#print(type(y))
+
+x=[1,2,3,4,5]
+print(len(x))
+c=(x[0]**3,x[1]**3,x[2]**3,x[3]**3,x[4]**3)
+x=tuple(c)
+print(c)
+
