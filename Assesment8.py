29b549963611586b77815fe54c5b79c0cd180f9a
swethamadhavi <swethamadhaviyadagiri2001@gmail.com>
2022-06-20 14:45:41 +0530

    Assesment8

---
+++ Assesment8.py
@@ -0,0 +1,48 @@
+#n=int(input("Enter a value"))
+#for n in range(1,n+1,2):
+ #      print("{0}".format(n))
+
+#num=int(input("Enter num value: "))
+#x=1
+#while(num>=0):
+ #   print(num,end=' ')
+  #  num=num-1
+
+#x=range(1,10)
+#print(sum(x))
+
+#x=[1,2,3,4,5,6,7,8,9,10]
+#print("sum", sum(x))
+#l=len(x)-1
+#s=0
+#while(l>0):
+ #   s=s+x[l]
+  #  l-=1
+#print("Sum of x:",s)
+
+#n=6
+#f=1
+#for i in range(1,n+1):
+ #   f=f*i
+#print("Factorial of n is",f)
+
+#n=int(input("enter a factor:"))
+#print("The factors of {} are,".format(n))
+#for i in range(1,n+1):
+ #   if(n%i==0):
+  #      print(i)
+
+#x=[10,1,2,3,100]
+#x[0],x[4]=x[4],x[0]
+#print(x)
+
+#x=[1,2,10,3,20,4]
+#odd_sum=0
+#even_sum=0
+#for j in range(len(x)):
+#        if(x[j]%2==0):
+ #         even_sum=even_sum+x[j]
+  #      else:
+   #       odd_sum=odd_sum+x[j]
+#print("Even number sum: ",even_sum)
+#print("odd number sum: ",odd_sum)
