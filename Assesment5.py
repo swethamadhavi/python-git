95aa6f5bae30bf660e4dfc532f5f90f16909d7e9
swethamadhavi <swethamadhaviyadagiri2001@gmail.com>
2022-06-20 14:41:03 +0530

    Assesment5

---
+++ Assesment5.py
@@ -0,0 +1,84 @@
+#a=10
+#b=20
+#print(a>b)
+#print(a<b)
+#print(a<=b)
+#print(a>=b)
+#print(a>=8)
+#print(a<=6)
+#print(b<=10)
+#print(b>=9)
+
+#a=12
+#b=2
+#print(a>=10 and b<=3)
+#print(a<=10 or b>=5)
+#print(not a)
+
+#a=122
+#print(chr(a))
+#a='g'
+#print(ord(a))
+
+#x=3
+#x=pow(3,3)
+#print(x)
+
+#x=[1,2,3,4,4,5]
+#print(x[1:5])
+#print(x[1:len(x)-1])
+
+#x=[1,2,3,1,2,3]
+#x=set(x)
+#print(x)
+#x=tuple(x)
+#print(x)
+
+#x={1,2,3,4,1,2,3,4,5}
+#x.add(36)
+#print(x)
+
+#x={'A':'Swetha','B':'madhavi'}
+#print(type(x))
+#print(x['A'])
+#print(x.keys())
+#print(x.values())
+
+#x=10
+#y=20
+#c=x if x>y else y
+#print(c)
+
+#a=1
+#b=20
+#c=(a and b)
+#print(c)
+
+#a=15
+#b=20
+#print(a+b)
+#print(a-b)
+#print(a*b)
+#print(a/b)
+#print(a//b)
+#print(a%b)
+#print(a**b)
+
+#x="python"
+#print(x*3)
+#y=3
+#print(x*y)
+
+#a=''
+#b=3
+#a=a*b
+#print(a)
+#print(len(a))
+
+#import math as m
+#a=123.5
+#print(m.floor(a))
+
+x=range(10,0,-1)
+for i in x:
+    print(i,end=" ")
