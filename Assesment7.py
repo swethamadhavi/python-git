0d23f8d0c7c1fb0abbb4a6630c303d8a94e6b500
swethamadhavi <swethamadhaviyadagiri2001@gmail.com>
2022-06-20 14:44:00 +0530

    Assesment7

---
+++ Assesment7.py
@@ -0,0 +1,109 @@
+#a=1
+#if(a<0):
+ #   print("Hello good morning ")
+  #  print("Hello World")
+#print("Welcome")
+
+#a=20
+#if(a==30):
+#    print("Hello")
+#    print("world")
+#    print("good morning")
+
+#a=5
+#if(a>=0):
+ #   print("+ve number")
+
+#a=int(input("Enter a value:"))
+#if(a>0):
+ #   print("+ve number")
+ #   print("Good Morning")
+#print("Hello World")
+
+#a=int(input("Enter a value:"))
+#if(a>=0):
+ #   print("Positive number")
+  #  print("Good Morning")
+#else:
+ #  print("Negative number")
+#print("Hello World")
+
+#a=int(input("Enter a value:"))
+#if(a%2==0):
+ #   print("Even number")
+  #  print("Good Morning")
+   # print("Hello World")
+#else:
+ #   print("Welcome to python")
+
+#a=24
+#b=34
+#if(a<b):
+ #   print("a value is",a)
+#else:
+   # print("b value is",b)
+#print("Good morning")
+
+#a=23
+#b=34
+#if(a>b):
+ #   print("Hello world")
+  #  print("Good morning")
+#elif(a==23):
+ #   print("Welcome")
+  #  print("python")
+#else:
+ #   print("Hello")
+
+#a=90
+#b=80
+#c=70
+#if(a<b and a>c):
+    #print("A grade")
+#elif(c>a):
+  #  print("B Grade")
+#elif(c>=b):
+ #   print("C Grade")
+#else:
+  #  print("D Grade")
+
+#a=20
+#b=40
+#if(a<b):
+ #   print("Hello World")
+   # print("Good morning")
+ #   if(a==20):
+   #     print("welcome")
+  #  elif(a<=10):
+     #   print("python")
+  #  else:
+   #     print("python is good")
+#else:
+ #   print("Java")
+
+
+a=457
+b=4324
+if(a<b):
+    print("Hello World")
+    print("good morning")
+    if(a!=3457):
+        print("welcome")
+        if(a<=4324):
+            print("python")
+            print("welcome")
+            if(a>b):
+                print("java")
+            elif(a!=b):
+                print("good")
+            else:
+                print("hi")
+        else:
+            print("Python is good")
+    else:
+        print("Iam a good")
+else:
+    print("java is good")
+
+
+
