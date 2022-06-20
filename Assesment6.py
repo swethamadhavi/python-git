833888ca6de594b875c191124a0c285da86b91a6
swethamadhavi <swethamadhaviyadagiri2001@gmail.com>
2022-06-20 14:42:43 +0530

    Assesment6

---
+++ Assesment6.py
@@ -0,0 +1,88 @@
+#check chr(97-32) is exists or not
+#x="BAD"
+#print(chr(97) in x)
+#print(chr(32) in x)
+
+#a=3
+#b=6
+#a=a^b
+#b=a^b
+#a=a^b
+#print("a val is",a)
+#print("b val is",b)
+
+#x=34
+#print(bin(34))
+#print(str(x))
+
+#x=["Swetha",10,17.5,"python"]
+#print(len(x))
+#x[0]=20
+#x[3]="Swetha"
+#print(x)
+
+#x=("swetha",10,19.5,"python")
+#print(len(x))
+#x=list(x)
+#x[0]="python"
+#x[3]="swetha"
+#print(x)
+#x=tuple(x)
+#print(x)
+
+#x={'A':100,'B':200,'A':300}
+#print(x['A'])
+
+#x=range(1,11,2)
+#for i in x:
+#    print(i,end=" ")
+
+#x=[1,2,3,4]
+#x[0]=x[0]**3
+#x[1]=x[1]**3
+#x[2]=x[2]**3
+#x[3]=x[3]**3
+#print(x)
+
+#x=[1,2,3,4]
+#x[0]=pow(x[0],3)
+#x[1]=pow(x[1],3)
+#x[2]=pow(x[2],3)
+#x[3]=pow(x[3],3)
+#print(x)
+
+#a=10
+#b=20
+#c=a if a>b else b
+#print(c)
+
+#x=list(range(1,11)) get only even numbers print in list
+#x=list(range(2,11,2))
+#print(x)
+
+#x=[1,2,3,4,3]
+#print(len(x))
+#print(x[::-1])
+#x.reverse()
+#print(x)
+
+#a=2
+#b=3
+#print(a<<b)
+
+#x="HELLO"
+#print(len(x))
+#print(x[1:3])
+#print(x[-4:-2])
+#print(x[1:len(x)-2])
+
+#x=[1,2,3,4,5,3,5]
+#print(len(x))
+#x=set(x)
+#print(x)
+#x=list(x)
+#print(x)
+
+#x="Swetha"
+#y="Madhavi"
+#print(x+y)
