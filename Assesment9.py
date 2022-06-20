7021f6b71af07041b70667f258a7bc03e424fd33
swethamadhavi <swethamadhaviyadagiri2001@gmail.com>
2022-06-20 14:47:10 +0530

    Assignment9

---
+++ Assesment9.py
@@ -0,0 +1,54 @@
+#for i in range(1,6):
+ #   for i in range(1,5):
+  #     if(i==5):
+   #       break
+    #   print(i,end=' ')
+    #print()
+
+#for i in range(1,6):
+ #   if(i==5):
+  #      continue
+  #  print(i,end=' ')
+   # print("Hello")
+#print()
+
+#x="python"
+#if(len(x)>=20):
+ #   pass
+#else:
+ #   print("Good Morning ")
+
+#x=[121,77,443,111,223,675]
+#s=1
+#temp=x
+#for i in x:
+ #   while(len(l)>0):
+  #      r=len(l)%10
+   #     s=s*10+r
+    #    x=len(l)//10
+#if(temp==s):
+ #   print(x)
+
+#for i in range(5, 0, -1):
+ #       print('*' * i, end='')
+  #      print()
+
+#x=int(input("Enter n value:"))
+#for i in range(1,x+1):
+ #   for i in range(1,x+1):
+  #      print('*',end='')
+   # print()
+
+
+#x=[1,2,3,2,3,4,5]
+#n=[]
+#count=0
+#for i in x:
+ #   if(i not in n):
+  #      count=count+1
+   #     n.append(i)
+#print(n)
+
+#x=["hello","world","good"]
+#print(x[0].capitalize(),x[1].capitalize(),x[2].capitalize())
+
